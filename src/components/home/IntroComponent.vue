b60da81fd4609b28aac477e08081f0a9e5d6a551
nickforlinux <inal.kharayev@outlook.com>
2025-11-05 14:44:44 +0400

    intro image resize

--- src/components/home/IntroComponent.vue
+++ src/components/home/IntroComponent.vue
@@ -1,5 +1,5 @@
 <template>
   <div
-    class="w-full h-82 shadow-xl bg-[url('@/assets/contents/intro/1.png')] bg-center bg-cover rounded-xl"
+    class="w-full h-36 md:h-82 shadow-xl bg-[url('@/assets/contents/intro/1.png')] bg-center bg-cover rounded-xl"
   ></div>
 </template>
